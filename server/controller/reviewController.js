2a617cb27999b5556a172724cdbe63f54718d6e9
Hyeongjin lee <gudwls2777@naver.com>
2025-05-21 23:11:11 +0900

    Feat: 리뷰 삭제, 수정 추가

--- server/controller/reviewController.js
+++ server/controller/reviewController.js
@@ -20,3 +20,54 @@ exports.createReview = async (req, res) => {
     res.status(500).json({ error: "리뷰 저장 실패" });
   }
 };
+
+exports.deleteReview = async (req, res) => {
+  try {
+    const reviewId = req.params.reviewId;
+    const userId = req.user._id;
+
+    const review = await Review.findById(reviewId);
+    if (!review) {
+      return res.status(404).json({ message: "리뷰가 존재하지 않습니다." });
+    }
+
+    if (review.author.toString() !== userId.toString()) {
+      return res.status(403).json({ message: "리뷰 삭제 권한이 없습니다." });
+    }
+
+    await Review.findByIdAndDelete(reviewId);
+
+    res.status(200).json({ message: "리뷰 삭제 성공" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "리뷰 삭제 실패" });
+  }
+};
+
+exports.updateReview = async (req, res) => {
+  try {
+    const reviewId = req.params.reviewId;
+    const userId = req.user._id;
+    const { content, keywords } = req.body;
+
+    const review = await Review.findById(reviewId);
+    if (!review) {
+      return res.status(404).json({ message: "리뷰를 찾을 수 없습니다." });
+    }
+
+    if (review.author.toString() !== userId.toString()) {
+      return res.status(403).json({ message: "수정 권한이 없습니다." });
+    }
+
+    // 수정할 필드만 변경
+    if (content !== undefined) review.content = content;
+    if (keywords !== undefined) review.keywords = keywords;
+
+    await review.save();
+
+    res.status(200).json({ message: "리뷰 수정 완료", review });
+  } catch (err) {
+    console.error("❌ 리뷰 수정 실패:", err);
+    res.status(500).json({ error: "리뷰 수정 실패", detail: err.message });
+  }
+};
--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -4,5 +4,7 @@ const auth = require("../middlewares/authMiddleware");
 const reviewController = require("../controller/reviewController");
 
 router.post("/review/:locationId", auth, reviewController.createReview); // POST 요청으로 리뷰 저장
+router.delete("/review/:reviewId", auth, reviewController.deleteReview); // DELETE 요청으로 리뷰 삭제
+router.patch("/review/:reviewId", auth, reviewController.updateReview);
 
 module.exports = router;
